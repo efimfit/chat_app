a575474638254b7fa76ff5120ba6d83c914b0440
Efim Khorunzhiy <fimkodh@mail.ru>
2023-02-01 22:37:08 +0100

    lib_uploaded

---
+++ lib/chat/view/new_message.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  var _enteredMessage = '';
+  final _controller = TextEditingController();
+
+  void _sendMessage() async {
+    // final fcmToken = await FirebaseMessaging.instance.getToken();
+    // print(fcmToken);
+    FocusScope.of(context).unfocus();
+    final userId = FirebaseAuth.instance.currentUser!.uid;
+    final userData =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+
+    FirebaseFirestore.instance.collection('chat').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': userId,
+      'username': userData['username'],
+      'userImage': userData['imageUrl'],
+    });
+    _controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              textCapitalization: TextCapitalization.sentences,
+              decoration: const InputDecoration(labelText: 'Send a message...'),
+              onChanged: (value) {
+                setState(() {
+                  _enteredMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            onPressed: _enteredMessage.isEmpty ? null : _sendMessage,
+            icon: Icon(
+              Icons.send,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
